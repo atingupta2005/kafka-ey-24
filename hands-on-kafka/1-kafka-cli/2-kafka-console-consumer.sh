cbb82866f6e1affb666d7df4bfc23e74be6e2719
Atin Gupta <atingupta@email.com>
2024-02-17 14:08:27 +0530

    -

--- hands-on-kafka/1-kafka-cli/2-kafka-console-consumer.sh
+++ hands-on-kafka/1-kafka-cli/2-kafka-console-consumer.sh
@@ -1,14 +1,14 @@
 # create a topic with 3 partitions
-kafka-topics.sh --bootstrap-server localhost:9092 --topic second_topic --create --partitions 3
+/usr/local/kafka/kafka-topics.sh --bootstrap-server localhost:9092 --topic second_topic --create --partitions 3
 
 # consuming
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic second_topic
+/usr/local/kafka/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic second_topic
 
 # other terminal
-kafka-console-producer.sh --bootstrap-server localhost:9092 --producer-property partitioner.class=org.apache.kafka.clients.producer.RoundRobinPartitioner --topic second_topic
+/usr/local/kafka/kafka-console-producer.sh --bootstrap-server localhost:9092 --producer-property partitioner.class=org.apache.kafka.clients.producer.RoundRobinPartitioner --topic second_topic
 
 # consuming from beginning
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic second_topic --from-beginning
+/usr/local/kafka/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic second_topic --from-beginning
 
 # display key, values and timestamp in consumer
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic second_topic --formatter kafka.tools.DefaultMessageFormatter --property print.timestamp=true --property print.key=true --property print.value=true --property print.partition=true --from-beginning
+/usr/local/kafka/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic second_topic --formatter kafka.tools.DefaultMessageFormatter --property print.timestamp=true --property print.key=true --property print.value=true --property print.partition=true --from-beginning
