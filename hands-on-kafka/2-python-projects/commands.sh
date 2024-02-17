285004613d69e275fd049ca4bad4f1cf30ed5df6
Atin Gupta <atingupta@email.com>
2024-02-17 21:13:10 +0530

    -

--- hands-on-kafka/2-python-projects/commands.sh
+++ hands-on-kafka/2-python-projects/commands.sh
@@ -5,3 +5,8 @@ cd 1-python-kafka-admin-api-simple
 #pip install -r requirements.txt
 #uvicorn main:app --reload --host 0.0.0.0 --port 8587
 
+rm -rf ~/kafka-ey-24
+cd ~
+git clone https://github.com/atingupta2005/kafka-ey-24
+cd kafka-ey-24/hands-on-kafka/1-kafka-cli
+
