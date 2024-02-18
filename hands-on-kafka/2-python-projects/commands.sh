9bae11e31ebf2810603e032506372cb1a1d8c3bb
Atin Gupta <atingupta@email.com>
2024-02-18 12:57:00 +0530

    -

--- hands-on-kafka/2-python-projects/commands.sh
+++ hands-on-kafka/2-python-projects/commands.sh
@@ -9,37 +9,39 @@ nc -vz localhost 2181
 nc -vz localhost 9092
 sudo apt install httpie  -y
 
+USER_SUFFIX=$(echo "$USER" | cut -c 2-5)
+echo $USER_SUFFIX
 ##----------------------------------------------------------------##
 # Create Topic
 cd ~/kafka-ey-24/hands-on-kafka/2-python-projects/1-python-kafka-admin-api-simple
 pip install -r requirements.txt
 
-uvicorn main:app --reload --host 0.0.0.0 --port 8587 > output.log 2>&1 &
-curl localhost:8587/hello-world
+uvicorn main:app --reload --host 0.0.0.0 --port 85$USER_SUFFIX > output.log 2>&1 &
+curl localhost:85$USER_SUFFIX/hello-world
 pkill uvicorn
 ##----------------------------------------------------------------##
 # Create Topic with 'retention.ms': '360000'
 cd ~/kafka-ey-24/hands-on-kafka/2-python-projects/2-python-kafka-admin-api-adv
 pip install -r requirements.txt
 
-uvicorn main:app --reload --host 0.0.0.0 --port 8587 > output.log 2>&1 &
-curl localhost:8587/hello-world
+uvicorn main:app --reload --host 0.0.0.0 --port 85$USER_SUFFIX > output.log 2>&1 &
+curl localhost:85$USER_SUFFIX/hello-world
 pkill uvicorn
 ##----------------------------------------------------------------##
 # Create message when we call api using POST - /api/people
 cd ~/kafka-ey-24/hands-on-kafka/2-python-projects/5-peopleservice-basic-producer
 pip install -r requirements.txt
 
-uvicorn main:app --reload --host 0.0.0.0 --port 8587 > output.log 2>&1 &
-http POST :8587/api/people count:=5
+uvicorn main:app --reload --host 0.0.0.0 --port 85$USER_SUFFIX > output.log 2>&1 &
+http POST :85$USER_SUFFIX/api/people count:=5
 pkill uvicorn
 ##----------------------------------------------------------------##
 # Create message when we call api using POST - /api/people with callbacks
 cd ~/kafka-ey-24/hands-on-kafka/2-python-projects/8-peopleservice-producer-python-adv
 pip install -r requirements.txt
 
-uvicorn main:app --reload --host 0.0.0.0 --port 8587 > output.log 2>&1 &
-http POST :8587/api/people count:=5
+uvicorn main:app --reload --host 0.0.0.0 --port 85$USER_SUFFIX > output.log 2>&1 &
+http POST :85$USER_SUFFIX/api/people count:=5
 tail output.log
 ##----------------------------------------------------------------##
 # Kafka consumer
@@ -47,7 +49,7 @@ cd ~/kafka-ey-24/hands-on-kafka/2-python-projects/10-peopleconsumeradv
 pip install -r requirements.txt
 
 python pyconsumer.py &
-http POST :8587/api/people count:=5
+http POST :85$USER_SUFFIX/api/people count:=5
 pkill uvicorn
 pkill pyconsumer
 pkill pyconsumer.py
@@ -56,15 +58,15 @@ pkill pyconsumer.py
 cd ~/kafka-ey-24/hands-on-kafka/2-python-projects/15-avro-people-service-producer
 pip install -r requirements.txt
 
-uvicorn main:app --reload --host 0.0.0.0 --port 8587 > output.log 2>&1 &
-http POST :8587/api/people count:=5
+uvicorn main:app --reload --host 0.0.0.0 --port 85$USER_SUFFIX > output.log 2>&1 &
+http POST :85$USER_SUFFIX/api/people count:=5
 ##----------------------------------------------------------------##
 # Kafka consumer with AVRO de-serialization
 cd ~/kafka-ey-24/hands-on-kafka/2-python-projects/16-avro-people-service-consumer
 pip install -r requirements.txt
 
 python pyconsumer.py &
-http POST :8587/api/people count:=1
+http POST :85$USER_SUFFIX/api/people count:=1
 pkill uvicorn
 pkill pyconsumer
 pkill pyconsumer.py
@@ -73,10 +75,10 @@ pkill pyconsumer.py
 cd ~/kafka-ey-24/hands-on-kafka/2-python-projects/17-avro-people-service-schema-evolution
 pip install -r requirements.txt
 
-uvicorn main:app --reload --host 0.0.0.0 --port 8587 > output.log 2>&1 &
-http POST :8587/api/people count:=1
+uvicorn main:app --reload --host 0.0.0.0 --port 85$USER_SUFFIX > output.log 2>&1 &
+http POST :85$USER_SUFFIX/api/people count:=1
 python pyconsumer.py &
-http POST :8587/api/people count:=1
+http POST :85$USER_SUFFIX/api/people count:=1
 pkill uvicorn
 pkill pyconsumer
 ##----------------------------------------------------------------##
