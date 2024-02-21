fc9464a574758feae67212fd3887cb8aa6d65ea6
Atin Gupta <atingupta@email.com>
2024-02-21 21:46:59 +0530

    -

--- setup-kafka/setup-kafka-zk-cluster/05-2-setup-broker-node-2.sh
+++ setup-kafka/setup-kafka-zk-cluster/05-2-setup-broker-node-2.sh
@@ -9,6 +9,8 @@ wget https://raw.githubusercontent.com/atingupta2005/kafka-ey-24/main/setup-kafk
 
 cp server-2.properties /usr/local/kafka/config/server.properties
 
+cat /usr/local/kafka/config/server.properties
+
 /usr/local/kafka/bin/kafka-server-start.sh  -daemon /usr/local/kafka/config/server.properties
 
 sleep 10
@@ -16,3 +18,6 @@ sleep 10
 nc -vz kafka1 9092
 nc -vz kafka2 9092
 nc -vz kafka3 9092
+
+tail -n 100 /usr/local/kafka/logs/server.log
+
