48216a180af613257b17c160e2bbe388637ceaeb
Atin Gupta <atingupta@email.com>
2024-02-24 16:03:14 +0530

    -

--- setup-kafka/setup-kafka-zk-cluster/07-restart-zk-kafka.sh
+++ setup-kafka/setup-kafka-zk-cluster/07-restart-zk-kafka.sh
@@ -8,6 +8,10 @@ sleep 5
 
 tail /usr/local/kafka/logs/zookeeper.out
 
+nc -vz zookeeper1 2181
+nc -vz zookeeper2 2181
+nc -vz zookeeper3 2181
+
 /usr/local/kafka/bin/zookeeper-server-start.sh  -daemon  /usr/local/kafka/config/zookeeper.properties
 
 sleep 5
@@ -17,6 +21,10 @@ sleep 5
 sleep 5
 
 tail -n 100 /usr/local/kafka/logs/server.log
+
+nc -vz kafka1 9092
+nc -vz kafka2 9092
+nc -vz kafka3 9092
 
 /usr/local/kafka/bin/kafka-topics.sh --bootstrap-server kafka1:9092 --list
 
