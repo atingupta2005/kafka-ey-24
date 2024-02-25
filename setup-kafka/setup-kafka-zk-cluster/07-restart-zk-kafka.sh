9c8dd0db618423052041d4c1f14f058e1dddbeea
atingupta2005 <atingupta2005@gmail.com>
2024-02-25 10:58:36 +0530

    Update 07-restart-zk-kafka.sh

--- setup-kafka/setup-kafka-zk-cluster/07-restart-zk-kafka.sh
+++ setup-kafka/setup-kafka-zk-cluster/07-restart-zk-kafka.sh
@@ -1,27 +1,17 @@
 /usr/local/kafka/bin/kafka-server-stop.sh /usr/local/kafka/config/server.properties
-
 sleep 5
-
 /usr/local/kafka/bin/zookeeper-server-stop.sh /usr/local/kafka/config/zookeeper.properties
-
-sleep 5
-
+sleep 10
+/usr/local/kafka/bin/zookeeper-server-start.sh  -daemon  /usr/local/kafka/config/zookeeper.properties
+sleep 20
 tail /usr/local/kafka/logs/zookeeper.out
-
 nc -vz zookeeper1 2181
 nc -vz zookeeper2 2181
 nc -vz zookeeper3 2181
 
-/usr/local/kafka/bin/zookeeper-server-start.sh  -daemon  /usr/local/kafka/config/zookeeper.properties
-
-sleep 5
-
 /usr/local/kafka/bin/kafka-server-start.sh  -daemon /usr/local/kafka/config/server.properties
-
-sleep 5
-
+sleep 10
 tail -n 100 /usr/local/kafka/logs/server.log
-
 nc -vz kafka1 9092
 nc -vz kafka2 9092
 nc -vz kafka3 9092
