b46b721d127e368035d226c6267db6d0f9c2a46a
Atin Gupta <atingupta@email.com>
2024-04-13 17:15:26 +0530

    -

--- setup-kafka/setup-kafka-zk-cluster/05-3setup-broker-node-3.sh
+++ setup-kafka/setup-kafka-zk-cluster/05-3setup-broker-node-3.sh
@@ -9,7 +9,6 @@ wget https://raw.githubusercontent.com/atingupta2005/kafka-ey-24/main/setup-kafk
 cp server-3.properties /usr/local/kafka/config/server.properties
 
 cat /usr/local/kafka/config/server.properties
-nano /usr/local/kafka/config/server.properties
 
 /usr/local/kafka/bin/kafka-server-start.sh  -daemon /usr/local/kafka/config/server.properties
 
